fe098248ffe8365431760b1a8f8929fee3aefd04
Maegan Womble <maeganwomble@gmail.com>
2020-05-12 16:09:42 -0500

    arrays and foreach

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -6,6 +6,8 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
+            // basic math operations and types
+
             float x = 22.1F;
             float y = 33.1F;
             Console.WriteLine(x + y);
@@ -13,6 +15,21 @@ namespace Gradebook
             var a = 22.1;
             var b = 33.1;
             Console.WriteLine(a + b);
+
+            // arrays and foreach
+
+            var arr = new double[3] { 1.12, 2.3, 4.5 }; // array of three doubles - numbers in {} are the initialized values
+            var arr1 = new double[] { 1.12, 2.3, 4.5 }; // also valid
+            var arr2 = new[] { 1.12, 2.3, 4.5 }; // also valid
+            var result = 0.0;
+
+            foreach (double number in arr)
+            {
+                result += number;
+            }
+            Console.WriteLine($"Result: {result}");
+
+            // if/else condition and string interpolation
 
             if (args.Length > 0)
             {
