d585c5eb0411ab8998c2db7f7e59c0725c8c7bc1
Maegan Womble <maeganwomble@gmail.com>
2020-05-12 15:59:32 -0500

    math and implicit typing

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -6,6 +6,14 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
+            float x = 22.1F;
+            float y = 33.1F;
+            Console.WriteLine(x + y);
+
+            var a = 22.1;
+            var b = 33.1;
+            Console.WriteLine(a + b);
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]}!");
