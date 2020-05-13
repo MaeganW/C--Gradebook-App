bb9b847c752d810888f828cac478f00046688e21
Maegan Womble <maeganwomble@gmail.com>
2020-05-13 15:38:03 -0500

    ref vs value type testing

--- test/Gradebook.Tests/TypeTests.cs
+++ test/Gradebook.Tests/TypeTests.cs
@@ -6,14 +6,24 @@ namespace Gradebook.Tests
     public class TypeTests
     {
         [Fact]
-        public void Test2()
+        public void GetBookReturnsDifferentObjects()
         {
-            // arrange
             var book1 = GetBook("New Book");
             var book2 = GetBook("Newer Book");
 
             Assert.Equal("New Book", book1.Name);
             Assert.Equal("Newer Book", book2.Name);
+        }
+
+        [Fact]
+        public void TwoVarsCanReferenceSameObject()
+        {
+            var book1 = GetBook("New Book");
+            var book2 = book1;
+
+            Assert.Equal(book2.Name, book1.Name);
+            Assert.Same(book1, book2);
+            Assert.True(Object.ReferenceEquals(book1, book2));
         }
 
         private Book GetBook(string name)
