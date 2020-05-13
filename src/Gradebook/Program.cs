2a7438856340dd9f3c2a258be307eecb3768a955
Maegan Womble <maeganwomble@gmail.com>
2020-05-13 09:27:39 -0500

    lists, foreach, and math

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Gradebook
 {
@@ -16,18 +17,40 @@ namespace Gradebook
             var b = 33.1;
             Console.WriteLine(a + b);
 
-            // arrays and foreach
+            // arrays and foreach - arrays have a fixed size
 
             var arr = new double[3] { 1.12, 2.3, 4.5 }; // array of three doubles - numbers in {} are the initialized values
+            arr[0] = 2.2;
             var arr1 = new double[] { 1.12, 2.3, 4.5 }; // also valid
             var arr2 = new[] { 1.12, 2.3, 4.5 }; // also valid
-            var result = 0.0;
 
+            var result = 0.0;
             foreach (double number in arr)
             {
                 result += number;
             }
             Console.WriteLine($"Result: {result}");
+
+            // Lists and Basic Math - lists are similar to arrays but dynamically sized
+
+            List<double> grades = new List<double>();
+            grades.Add(98.5);
+
+            var grades2 = new List<double>() { 75.0, 88.5 };
+            grades2.Add(90.5);
+
+            var length = grades2.Count;
+            Console.WriteLine($"Length of Grades2 List: {length}");
+
+            var result2 = 0.0;
+            foreach (var number in grades2)
+            {
+                result2 += number;
+            }
+            Console.WriteLine($"Result2: {result2}");
+
+            var average = result2 / length;
+            Console.WriteLine($"Average of grades2: {average}");
 
             // if/else condition and string interpolation
 
