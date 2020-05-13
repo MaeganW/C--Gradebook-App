cfd18ab9053d7cbcbb8c40d83a3b7f7085fc2d6e
Maegan Womble <maeganwomble@gmail.com>
2020-05-13 11:10:00 -0500

    access modifiers, this context, encapsulation

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -4,15 +4,17 @@ namespace Gradebook
 {
     class Book
     {
-        public Book()
+        public Book(string name)
         {
             grades = new List<double>();
+            this.name = name; // normally "this" is implicit, but because "name" is a param, we need to be explicit
         }
         public void addGrade(double grade)
         {
-            grades.Add(grade);
+            grades.Add(grade); // "this" in this.grades is implicit
         }
 
-        List<double> grades; // this is a field on the class/type - cannot be implicitly typed
+        private List<double> grades; // this is a field on the class/type - cannot be implicitly typed
+        private string name; // make fields private for better encapsulation
     }
 }
