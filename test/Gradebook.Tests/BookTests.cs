5c48a6ab055e21ce424aa7c203b703b1a4abfb7f
Maegan Womble <maeganwomble@gmail.com>
2020-05-13 14:47:46 -0500

    added unit testing and inital test for statistics method

---
+++ test/Gradebook.Tests/BookTests.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+
+namespace Gradebook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // arrange
+            var book = new Book("New Book");
+            book.AddGrade(22.3);
+            book.AddGrade(92.3);
+            book.AddGrade(88.3);
+
+            // act
+            var result = book.GetStatistics();
+
+            // assert
+            Assert.Equal(22.3, result.LowestGrade, 1);
+            Assert.Equal(92.3, result.HighestGrade, 1);
+            Assert.Equal(67.6, result.AverageGrade, 1);
+        }
+    }
+}
