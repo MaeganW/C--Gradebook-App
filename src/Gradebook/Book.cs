a9c94074b98fd9943a9e2cd81c66e81b389a10b0
Maegan Womble <maeganwomble@gmail.com>
2020-05-13 12:00:51 -0500

    encapsulation practices continued

--- src/Gradebook/Book.cs
+++ src/Gradebook/Book.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Gradebook
@@ -12,6 +13,47 @@ namespace Gradebook
         public void addGrade(double grade)
         {
             grades.Add(grade); // "this" in this.grades is implicit
+        }
+
+        public double getHighestGrade()
+        {
+            var highestGrade = double.MinValue;
+            foreach (var grade in this.grades)
+            {
+                // if (grade > highestGrade)
+                // {
+                //     highestGrade = grade;
+                // }
+                highestGrade = Math.Max(grade, highestGrade);
+            }
+            return highestGrade;
+        }
+
+        public double getLowestGrade()
+        {
+            var lowestGrade = double.MaxValue;
+            foreach (var grade in this.grades)
+            {
+                lowestGrade = Math.Min(grade, lowestGrade);
+            }
+            return lowestGrade;
+        }
+
+        public double getAverageGrade()
+        {
+            var average = 0.0;
+            foreach (var grade in this.grades)
+            {
+                average += grade;
+            }
+            return average /= this.grades.Count;
+        }
+
+        public void showStatistics()
+        {
+            Console.WriteLine($"The highest grade is {this.getHighestGrade()}");
+            Console.WriteLine($"The lowest grade is {this.getLowestGrade()}");
+            Console.WriteLine($"The average grade is {this.getAverageGrade():N1}");
         }
 
         private List<double> grades; // this is a field on the class/type - cannot be implicitly typed
--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -7,13 +7,17 @@ namespace Gradebook
     {
         static void Main(string[] args)
         {
-            var book = new Book();
+            var book = new Book("Maegan's Grade Book");
             book.addGrade(45.0);
+            book.addGrade(90.0);
+            book.addGrade(75.5);
+
+            book.showStatistics();
 
             // misc practice fns
 
             var practice = new PracticeProblems();
-            practice.run(new[] { "Maegan", "Diana" });
+            // practice.run(new[] { "Maegan", "Diana" });
         }
     }
 }
