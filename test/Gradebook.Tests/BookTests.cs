5c1ba5898e494c009156b48a5bca73d8e223a018
Maegan Womble <maeganwomble@gmail.com>
2020-05-14 13:16:50 -0500

    added testing for average letter grade

--- test/Gradebook.Tests/BookTests.cs
+++ test/Gradebook.Tests/BookTests.cs
@@ -22,5 +22,20 @@ namespace Gradebook.Tests
             Assert.Equal(92.3, result.HighestGrade, 1);
             Assert.Equal(67.6, result.AverageGrade, 1);
         }
+
+        [Fact]
+        public void BookGeneratesCorrectLetterGrade()
+        {
+            var book = new Book("New Book");
+            book.AddGrade(75.0);
+            book.AddGrade(55.0);
+            book.AddGrade(95.0);
+
+            var result = book.GetStatistics();
+
+            // Assert.Equal()
+            Console.WriteLine($"Average Letter Grade: ", result.AverageLetterGrade);
+            Console.WriteLine($"Average Grade: ", result.AverageGrade);
+        }
     }
 }
