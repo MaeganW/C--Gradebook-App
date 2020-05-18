8f7c314ad58389bab7706edeec63bc9a994b4436
Maegan Womble <maeganwomble@gmail.com>
2020-05-18 13:00:02 -0500

    refactored Statistics class

--- src/Gradebook/Statistics.cs
+++ src/Gradebook/Statistics.cs
@@ -1,10 +1,54 @@
+using System;
+
 namespace Gradebook
 {
     public class Statistics
     {
-        public double AverageGrade;
+        public double AverageGrade
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
         public double HighestGrade;
         public double LowestGrade;
-        public char AverageLetterGrade;
+        public char AverageLetterGrade
+        {
+            get
+            {
+                switch (AverageGrade)
+                {
+                    case var g when g >= 90:
+                        return 'A';
+                    case var g when g >= 80:
+                        return 'B';
+                    case var g when g >= 70:
+                        return 'C';
+                    case var g when g >= 60:
+                        return 'D';
+                    default:
+                        return 'F';
+                }
+            }
+        }
+        public double Sum;
+        public int Count;
+
+        public void Add(double grade)
+        {
+            HighestGrade = Math.Max(grade, HighestGrade);
+            LowestGrade = Math.Min(grade, LowestGrade);
+            Sum += grade;
+            Count++;
+        }
+
+        public Statistics()
+        {
+            HighestGrade = double.MaxValue;
+            LowestGrade = double.MinValue;
+            Sum = 0.0;
+            Count = 0;
+        }
     }
 }
